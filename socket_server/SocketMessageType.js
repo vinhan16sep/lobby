13ad61c26a462f9fc4d40e244c3b8c9403a56dec
tanpa <tanpham.hust@gmail.com>
2021-11-21 00:39:31 +0700

    user list, user join, user leave message

--- socket_server/SocketMessageType.js
+++ socket_server/SocketMessageType.js
@@ -1,6 +1,8 @@
 var SocketMessageType = {
     JOIN: "JOIN",
     SEND_MESSAGE: "SEND_MESSAGE",
-    RECV_MESSAGE: "RECV_MESSAGE"
+    RECV_MESSAGE: "RECV_MESSAGE",
+    USER_LEAVE: "USER_LEAVE",
+    USER_JOIN: "USER_JOIN"
 };
 module.exports = SocketMessageType;
--- socket_server/UserConnection.js
+++ socket_server/UserConnection.js
@@ -1,11 +1,19 @@
 class UserConnection {
-    constructor(userId, socketId) {
+    constructor(userId, socketId, name) {
         this.userId = userId;
         this.socketId = socketId;
+        this.name = name;
     }
 
     static fromJSON(data) {
         return Object.assign(new UserConnection(), data);
+    }
+
+    toJSON() {
+        return {
+            userId: this.userId,
+            name: this.name
+        };
     }
 
     isSameSocketId(socketId) {
