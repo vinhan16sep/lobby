523a81307a6d354fb049cd75d2e2cf73e48b3ccd
hunglq-at-096056044497 <luongquochung@gmail.com>
2021-11-30 00:35:55 +0700

    update UI 20211129

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,71 +8,65 @@
 @endsection
 
 @section('view')
-    <div class="view-home" style="background-image: url('{{ asset("public/img/Wallpaper.jpg") }}')">
-        <div class="overlay">
-            <div class="form-login">
-                <div class="panel panel-default">
-                    <div class="row">
-                        @if(Session::has('error'))
-                            <p class="alert {{ Session::get('alert-class', 'alert-warning') }}">{{ Session::get('error') }}<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></p>
-                        @endif
-                        @if(Session::has('success'))
-                            <p class="alert {{ Session::get('alert-class', 'alert-success') }}">{{ Session::get('success') }}<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></p>
-                        @endif
-                    </div>
-                    <div class="panel-heading">Đăng nhập</div>
-                    <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                            {{ csrf_field() }}
-    
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="control-label">Địa chỉ email</label>
-    
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-    
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-    
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="control-label">Mật khẩu</label>
-    
-                                <input id="password" type="password" class="form-control" name="password" required>
-    
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-    
-                            {{-- <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                        </label>
+    <div class="view-home">
+        <div class="row g-0">
+            <div class="col-md-8">
+                <div class="img-mask">
+                    <img src="{{ asset("img/banner_login.jpg") }}" alt="Banner of Login">
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="form-login">
+                    <div class="panel panel-default">
+                        <div class="row">
+                            @if(Session::has('error'))
+                                <p class="alert {{ Session::get('alert-class', 'alert-warning') }}">{{ Session::get('error') }}<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></p>
+                            @endif
+                            @if(Session::has('success'))
+                                <p class="alert {{ Session::get('alert-class', 'alert-success') }}">{{ Session::get('success') }}<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></p>
+                            @endif
+                        </div>
+                        <div class="panel-heading">Đăng nhập</div>
+                        <div class="panel-body">
+                            <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                                {{ csrf_field() }}
+        
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <label for="email" class="control-label">Địa chỉ email</label>
+        
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+        
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+        
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                    <label for="password" class="control-label">Mật khẩu</label>
+        
+                                    <input id="password" type="password" class="form-control" name="password" required>
+        
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+        
+                                <div class="form-group">
+                                    <div class="col-md-12">
+                                        <button class="btn btn-block btn-primary" type="submit">
+                                            Đăng nhập
+                                        </button>
                                     </div>
                                 </div>
-                            </div> --}}
-    
-                            <div class="form-group">
-                                <div class="col-md-12">
-                                    <button class="btn btn-block btn-primary" type="submit">
-                                        Đăng nhập
-                                    </button>
-    
-                                    {{-- <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        Forgot Your Password?
-                                    </a> --}}
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
-                </div>
+                </div>  
             </div>
         </div>
     </div>
--- socket_server/Logger.js
+++ socket_server/Logger.js
@@ -1,18 +1,18 @@
-var log4js = require("log4js");
-var path = require("path");
-var dateFormat = require("date-format");
+var log4js = require('log4js');
+var path = require('path');
+var dateFormat = require('date-format');
 class Logger {
     constructor(isDebug) {
         var _this = this;
         this.isDebug = isDebug;
-        log4js.addLayout("json", function (config) {
+        log4js.addLayout('json', function (config) {
             return function (logEvent) {
-                if (logEvent.data[0] == "PLAIN") {
+                if (logEvent.data[0] == 'PLAIN') {
                     return logEvent.data[3];
                 } else {
                     return JSON.stringify({
-                        time: dateFormat("yyyy-MM-dd hh:mm:ss.SSS", new Date()),
-                        host: _this.hostAddress || "",
+                        time: dateFormat('yyyy-MM-dd hh:mm:ss.SSS', new Date()),
+                        host: _this.hostAddress || '',
                         action: logEvent.data[0],
                         type: logEvent.data[1],
                         sessionId: logEvent.data[2],
@@ -22,50 +22,50 @@ class Logger {
                 }
             };
         });
-        log4js.configure(path.join(path.dirname(process.execPath), "/log4js-config.json"));
-        // log4js.configure("log4js-config.json");
-        this.logger = log4js.getLogger("logfile");
-        this.consoleLogger = log4js.getLogger("consoleout");
+        log4js.configure(path.join(path.dirname(process.execPath), '/log4js-config.json'));
+        // log4js.configure('log4js-config.json');
+        this.logger = log4js.getLogger('logfile');
+        this.consoleLogger = log4js.getLogger('consoleout');
     }
 
     trace(action, type, sessionId, ...desc) {
         if (this.isDebug) {
-            this.consoleLogger.trace(action, type, sessionId, desc.join(" "));
+            this.consoleLogger.trace(action, type, sessionId, desc.join(' '));
         }
-        this.logger.trace(action, type, sessionId, desc.join(" "));
+        this.logger.trace(action, type, sessionId, desc.join(' '));
     }
     debug(action, type, sessionId, ...desc) {
         if (this.isDebug) {
-            this.consoleLogger.debug(action, type, sessionId, desc.join(" "));
+            this.consoleLogger.debug(action, type, sessionId, desc.join(' '));
         }
-        this.logger.debug(action, type, sessionId, desc.join(" "));
+        this.logger.debug(action, type, sessionId, desc.join(' '));
     }
     info(action, type, sessionId, ...desc) {
         if (this.isDebug) {
-            this.consoleLogger.info(action, type, sessionId, desc.join(" "));
+            this.consoleLogger.info(action, type, sessionId, desc.join(' '));
         }
-        this.logger.info(action, type, sessionId, desc.join(" "));
+        this.logger.info(action, type, sessionId, desc.join(' '));
     }
     warn(action, type, sessionId, ...desc) {
         if (this.isDebug) {
-            this.consoleLogger.warn(action, type, sessionId, desc.join(" "));
+            this.consoleLogger.warn(action, type, sessionId, desc.join(' '));
             // console.log(msg);
         }
-        this.logger.warn(action, type, sessionId, desc.join(" "));
+        this.logger.warn(action, type, sessionId, desc.join(' '));
     }
     error(action, type, sessionId, ...desc) {
         if (this.isDebug) {
-            this.consoleLogger.error(action, type, sessionId, desc.join(" "));
+            this.consoleLogger.error(action, type, sessionId, desc.join(' '));
             // console.log(msg);
         }
-        this.logger.error(action, type, sessionId, desc.join(" "));
+        this.logger.error(action, type, sessionId, desc.join(' '));
     }
     fatal(action, type, sessionId, ...desc) {
         if (this.isDebug) {
-            this.consoleLogger.fatal(action, type, sessionId, desc.join(" "));
+            this.consoleLogger.fatal(action, type, sessionId, desc.join(' '));
             // console.log(msg);
         }
-        this.logger.fatal(action, type, sessionId, desc.join(" "));
+        this.logger.fatal(action, type, sessionId, desc.join(' '));
     }
 }
 module.exports = Logger;
