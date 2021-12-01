d58e6df037330ba7033f93047c787ca474f4bc4d
vinhan16sep <vinhan16sep@gmail.com>
2021-12-02 00:08:13 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-})->name('default');
+//Route::get('/', function () {
+//    return view('auth.login');
+//})->name('default');
+
+Route::get('/', 'Auth\LoginController@index')->name('default');
 
 Auth::routes();
 
